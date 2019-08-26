07d76eb8ca8d58dbc32270a1b384f2496dcb93ae
liyanmin <13824774439@163.com>
2019-08-27 00:20:05 +0800

    栈大小设置

---
+++ src/main/java/gc/parameters/TestStackDeep.java
@@ -0,0 +1,19 @@
+package gc.parameters;
+
+public class TestStackDeep {
+    private static int count = 1;
+
+    private static void recursive() {
+        count++;
+        recursive();
+    }
+
+    // -Xss160k  栈大小
+    public static void main(String[] args) {
+        try {
+            recursive();
+        } catch (Error e) {
+            System.out.println("count:" + count);
+        }
+    }
+}
