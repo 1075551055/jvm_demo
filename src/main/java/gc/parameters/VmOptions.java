2a61c2c57e899480014a3df053983a21f95dcf48
liyanmin <13824774439@163.com>
2019-09-04 22:38:00 +0800

    查看虚拟机显式和隐式参数

---
+++ src/main/java/gc/parameters/VmOptions.java
@@ -0,0 +1,9 @@
+package gc.parameters;
+
+public class VmOptions {
+    // -XX:+PrintVMOptions  显示显式参数
+    // -XX:+PrintCommandLineFlags 打印传递给虚拟机的显式参数和隐式参数
+    public static void main(String[] args) {
+
+    }
+}
