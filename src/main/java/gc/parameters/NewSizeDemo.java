a66f276ce9963eba5c67d7f93e9a36121f07520b
liyanmin <13824774439@163.com>
2019-09-08 10:53:47 +0800

    新生代参数设置

---
+++ src/main/java/gc/parameters/NewSizeDemo.java
@@ -0,0 +1,19 @@
+package gc.parameters;
+
+public class NewSizeDemo {
+    // -Xmn  设置新生代大小，一般设置为整个堆的1/3，1/4
+    // -XX:SurvivorRatio   设置新生代eden区与from/to survivor区的比例， = eden/from = eden/to
+    // todo 为什么新生代默认就使用了1m左右的空间？
+    public static void main(String[] args) {
+        // Java HotSpot(TM) 64-Bit Server VM warning: NewSize (1536k) is greater than the MaxNewSize (1024k). A new max generation size of 1536k will be used.
+        //-Xmx20m -Xms20m -Xmn1m -XX:SurvivorRatio=2 -XX:+PrintGCDetails
+
+
+        // -XX:PretenureSizeThreshold=3
+        // java -XX:+PrintFlagsFinal -XX:NewSize=1k -version | grep NewSize     ===>jdk 8
+        byte[] bytes = null;
+        for (int i = 0; i < 5; i++) {
+            bytes = new byte[1 * 1024 * 1024];
+        }
+    }
+}
--- src/main/java/gc/parameters/VmOptions.java
+++ src/main/java/gc/parameters/VmOptions.java
@@ -3,6 +3,7 @@ package gc.parameters;
 public class VmOptions {
     // -XX:+PrintVMOptions  显示显式参数
     // -XX:+PrintCommandLineFlags 打印传递给虚拟机的显式参数和隐式参数
+    // -XX:+PrintFlagsFinal 显示虚拟机所有参数
     public static void main(String[] args) {
 
     }
