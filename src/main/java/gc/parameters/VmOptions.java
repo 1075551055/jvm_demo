dcabc49d0777c08f030993097cc060c1ed4872d7
liyanmin <13824774439@163.com>
2019-09-14 17:39:35 +0800

    如何查看使用了什么类型的垃圾收集器

--- src/main/java/gc/parameters/VmOptions.java
+++ src/main/java/gc/parameters/VmOptions.java
@@ -4,6 +4,7 @@ public class VmOptions {
     // -XX:+PrintVMOptions  显示显式参数
     // -XX:+PrintCommandLineFlags 打印传递给虚拟机的显式参数和隐式参数
     // -XX:+PrintFlagsFinal 显示虚拟机所有参数
+    // java -XX:+PrintCommandLineFlags -version查看使用了什么垃圾收集器 https://stackoverflow.com/questions/2101518/difference-between-xxuseparallelgc-and-xxuseparnewgc
     public static void main(String[] args) {
 
     }
